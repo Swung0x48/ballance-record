bb47132d83246bb2376285f2e9517febdd4a65dd
Swung0x48 <swung0x48@outlook.com>
2021-03-09 08:20:31 +0800

    Preliminary i18n support

---
+++ src/config/i18n.ts
@@ -0,0 +1,20 @@
+import LanguageDetector from 'i18next-browser-languagedetector'
+import i18n from 'i18next'
+import { initReactI18next } from 'react-i18next'
+import Backend from 'i18next-http-backend'
+import resources from './lang'
+
+i18n
+    .use(Backend)
+    .use(LanguageDetector)
+    .use(initReactI18next)
+    .init({
+        resources,
+        fallbackLng: 'en',
+        keySeparator: false,
+        interpolation: {
+            escapeValue: false
+        }
+    });
+
+export default i18n
